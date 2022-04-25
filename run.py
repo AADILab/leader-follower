2e207450e52fc4ac451d43e9b37366ee5b7302b1
EverardoG <everardo.a.gonzalez@gmail.com>
2022-04-25 09:36:08 -0700

    initial commit

---
+++ run.py
@@ -0,0 +1,14 @@
+from env_lib import parallel_env, ROCK
+
+def policy(observation, agent):
+    return ROCK
+
+env = parallel_env()
+observations = env.reset()
+max_cycles = 1
+for step in range(max_cycles):
+    actions = {agent: policy(observations[agent], agent) for agent in env.agents}
+    print(actions)
+    observations, rewards, dones, infos = env.step(actions)
+
+print(env.agents)
