bac63836948e3f8061dfa9bd6714d5cc09d29e57
EverardoG <everardo.a.gonzalez@gmail.com>
2022-07-19 12:54:50 -0700

    added play_sim for playable boids game

--- env_lib.py
+++ env_lib.py
@@ -39,7 +39,7 @@ def raw_env():
 class BoidsEnv(ParallelEnv):
     metadata = {'render.modes': ['human', 'none'], "name": "boids"}
 
-    def __init__(self, num_leaders = 2, num_followers = 10, FPS = 60, positions = None, follower_inds = None, learning_module: LearningModule = None, num_steps = 20000, render_mode = 'human'):
+    def __init__(self, num_leaders = 2, num_followers = 10, FPS = 60, positions = None, headings = None, follower_inds = None, learning_module: LearningModule = None, num_steps = 20000, render_mode = 'human', map_size = np.array([50,50])):
         '''
         The init method takes in environment arguments and should define the following attributes:
         - possible_agents
@@ -56,9 +56,8 @@ class BoidsEnv(ParallelEnv):
         self.FPS = FPS
         self.dt = 1/float(FPS)
 
-        map_size = np.array([50,50])
         rs = (2,3,5)
-        self.bm = BoidsManager(num_leaders=num_leaders, num_followers=num_followers, max_velocity=10, max_angular_velocity=np.pi*0.5, radius_repulsion=rs[0], radius_orientation=rs[1], radius_attraction=rs[2], map_size=map_size, ghost_density=10, dt=self.dt, positions=positions)
+        self.bm = BoidsManager(num_leaders=num_leaders, num_followers=num_followers, max_velocity=10, max_angular_velocity=np.pi*0.5, radius_repulsion=rs[0], radius_orientation=rs[1], radius_attraction=rs[2], map_size=map_size, ghost_density=10, dt=self.dt, positions=positions, headings=headings)
 
         self.render_mode = render_mode
         if self.render_mode == 'none':
---
+++ play_sim.py
@@ -0,0 +1,68 @@
+import pygame
+from time import time
+import pygame
+
+from env_lib import BoidsEnv
+
+import numpy as np
+np.random.seed(3)
+
+def policy(observation, agent):
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[pygame.K_UP]:
+        vel = 10
+    elif pressed_keys[pygame.K_DOWN]:
+        vel = 0
+    else:
+        vel = 5
+    if pressed_keys[pygame.K_RIGHT]:
+        turn = -np.pi
+    elif pressed_keys[pygame.K_LEFT]:
+        turn = np.pi
+    else:
+        turn = 0
+    return np.array([turn, vel])
+
+
+num_followers = 100
+num_leaders = 10
+total_agents = num_followers + num_leaders
+map_size = np.array([100,100])
+
+# Setup positions so leaders are all centered
+positions = np.vstack((
+    np.hstack((
+                np.random.uniform(map_size[0], size=(num_followers,1)),
+                np.random.uniform(map_size[1], size=(num_followers,1))
+            )),
+    np.hstack((
+                np.ones((num_leaders, 1))*map_size[0]/2,
+                np.ones((num_leaders, 1))*map_size[1]/2
+            ))
+))
+# Modify leader positions so they are clustered around the same location
+positions[num_followers:, 0] += np.random.uniform(num_leaders, size=(num_leaders)) - num_leaders/2
+positions[num_followers:, 1] += np.random.uniform(num_leaders, size=(num_leaders)) - num_leaders/2
+
+# Setup headings and modify them so all leaders are aligned
+headings = np.random.uniform(0, 2*np.pi, size = (num_followers+num_leaders, 1))
+headings[num_followers:] = np.pi/2
+
+env = BoidsEnv(num_leaders = num_leaders, num_followers = num_followers, FPS=60, follower_inds=[], render_mode='human', map_size=map_size, positions = positions, headings = headings)
+
+# Reset env for first round of observations
+observations = env.reset()
+
+last_time = None
+shutdown = False
+while not shutdown:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            shutdown = True
+    current_time = time()
+    if last_time is None or current_time - last_time >= env.dt:
+        # if last_time is not None: print("t:", current_time-last_time-dt)
+        last_time = current_time
+        actions = {agent: policy(observations[agent], agent) for agent in env.possible_agents}
+        observations, rewards, dones, infos = env.step(actions)
+        env.render()
