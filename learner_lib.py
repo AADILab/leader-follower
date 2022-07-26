d65c00ae54751c8328a1d2d26f3cbb754d46ddd6
EverardoG <everardo.a.gonzalez@gmail.com>
2022-07-26 14:16:08 -0700

    multithreading with np arrays for networks

--- learner_lib.py
+++ learner_lib.py
@@ -1,45 +1,94 @@
+from ast import Call
 import queue
-from typing import List, Dict
+from typing import List, Dict, Optional, Callable
 import random
+import traceback
+from sys import exit
 
-import torch
-import torch.nn as nn
+# import torch
+# import torch.nn as nn
 from tqdm import tqdm
 import numpy as np
 from multiprocessing import Event, Process, Queue, Manager
 from time import time
+from network_lib import NN
 
 from env_lib import BoidsEnv
 
 # Genome encodes weights of a network as list of tensors
-Genome = List[torch.TensorType]
+# Genome = List[torch.TensorType]
+Genome = List[np.array]
 
 def generateSeed():
     return int((time() % 1) * 1000000)
 
-class FeedForwardNet(nn.Module):
-    def __init__(self, input_size, hid_size, out_size) -> None:
-        super(FeedForwardNet, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(input_size, hid_size),
-            nn.Tanh(),
-            nn.Linear(hid_size, out_size),
-            nn.Tanh()
-        )
-        self.model.requires_grad_(False)
+class Worker():
+    def __init__(self, in_queue: Queue, out_queue: Queue, stop_event: Event, id: int, env_kwargs: Dict = {}, nn_kwargs: Dict ={}):
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+        self.stop_event = stop_event
+        self.id = id
+        self.env = BoidsEnv(**env_kwargs)
+        self.net = NN(**nn_kwargs)
+        self.genome_id = None
 
-    def run(self, x) -> torch.TensorType:
-        return self.model(x)
+    def __call__(self):
+        try:
+            while not self.stop_event.is_set():
+                try:
+                    input = self.in_queue.get(timeout=0.01)
+                except queue.Empty:
+                    continue
 
-    def getWeights(self) -> Genome:
-        d = self.model.state_dict()
-        return [d['0.weight'], d['2.weight']]
+                self.genome_id = input["id"]
 
-    def setWeights(self, weights: Genome) -> None:
-        d = self.model.state_dict()
-        d['0.weight'] = weights[0]
-        d['2.weight'] = weights[1]
-        self.model.load_state_dict(d)
+                genome = input["genome"]
+                seed = input["seed"]
+
+                try:
+                    # fitness = self.evaluateGenome(genome, seed, False)
+                    fitness = self.evaluateGenome(genome, seed, False)
+
+                except AttributeError as e:
+                    print("AttributeError!")
+                    print(e)
+                    fitness = np.inf
+
+                output = {"id": input["id"], "fitness": fitness}
+                self.out_queue.put(output)
+
+        except KeyboardInterrupt:
+            print(f"Interrupt on Worker {self.id}, Genome {self.genome_id} !")
+        except Exception as e:
+            print(f"Error on Worker {self.id}, Genome {self.genome_id}! Exiting program. Error: {e}\nFull Traceback:\n{traceback.format_exc()}")
+            self.stop_event.set()
+        finally:
+            print(f"Shutting down Worker {self.id}")
+
+    def evaluateGenome(self, genome: Genome, seed: int = 0, draw: bool = False) -> float:
+        """Load genome into boids environment and calculate a fitness score."""
+        # Load network with weights from genome
+        self.net.setWeights(genome)
+
+        # Run network on boids environment
+        observations = self.env.reset()
+        done = False
+        cumulative_reward = 0
+        while not done:
+            if draw:
+                self.env.render()
+            # Collect actions for all agents with each agent using the same genome to guide its action
+            actions = {agent_name: self.net.forward(np.array(observations[agent_name])) for agent_name in self.env.possible_agents}
+            # Step forward the environment
+            observations, rewards, dones, _  = self.env.step(actions)
+            # Save done
+            done = True in dones.values()
+            # Add the team reward to the cumulative reward
+            # Need [0] index because rewards are an array of rewards. One for each objective.
+            cumulative_reward += rewards["team"][0]
+        self.env.close()
+
+        return cumulative_reward
 
 class Learner():
     def __init__(self, population_size: int, num_parents: int, sigma_mutation: float, num_workers: int = 10, env_kwargs: Dict = {"num_leaders": 10, "num_followers": 90, "FPS": 60, "num_steps": 60*60, "render_mode": 'none'}) -> None:
@@ -56,25 +105,33 @@ class Learner():
         self.population = [self.randomGenome() for _ in range(self.population_size)]
 
         # Initialize environment
-        self.env = BoidsEnv(**env_kwargs)
+        self.env_kwargs = env_kwargs
 
         # Process event - set flag to True to turn off workers
         self.stop_event = Event()
 
-        print("e: ", self.stop_event.is_set())
-
         self.work_queue = Queue(1000)
         self.fitness_queue = Queue(1000)
+        init_workers = [
+            Worker(
+                in_queue=self.work_queue,
+                out_queue=self.fitness_queue,
+                stop_event=self.stop_event,
+                id=worker_id,
+                env_kwargs=env_kwargs,
+                nn_kwargs={"num_inputs": self.input_size, "num_hidden": self.hidden_size, "num_outputs": self.out_size}
+            )
+            for worker_id in range(num_workers)
+        ]
         self.workers = [
             Process(
-                target=self.evaluationWorker,
-                args=(self.work_queue, self.fitness_queue, i, env_kwargs),
+                target=worker,
+                args=(),
             )
-            for i in range(num_workers)
+            for worker in init_workers
         ]
         for w in self.workers:
             w.start()
-
 
     def __del__(self):
         self.cleanup()
@@ -83,88 +140,26 @@ class Learner():
         # End work processes on deletion
         try:
             self.stop_event.set()
-            print("e2: ", self.stop_event.is_set())
             for w in self.workers:
                 w.join()
         except:
             pass
 
-    def createNet(self) -> FeedForwardNet:
-        return FeedForwardNet(self.input_size, self.hidden_size, self.out_size)
-
-    def randomGenome(self) -> Genome:
-        return [
-            torch.normal(mean=0.0, std=1.0, size=(self.hidden_size, self.input_size)),
-            torch.normal(mean=0.0, std=1.0, size=(self.out_size, self.hidden_size))
-            ]
-
-    def evaluationWorker(self, in_queue, out_queue, id=0, env_kwargs={}):
-        try:
-            while not self.stop_event.is_set():
-                try:
-                    input = in_queue.get(timeout=0.01)
-                except queue.Empty:
-                    continue
-
-                genome = input["genome"]
-                seed = input["seed"]
-
-                try:
-                    fitness = self.evaluateGenome(genome, seed, False)
-
-                except AttributeError as e:
-                    print(e)
-                    fitness = np.inf
-
-                output = {"id": input["id"], "fitness": fitness}
-                out_queue.put(output)
-
-        except KeyboardInterrupt:
-            print(f"Interrupt on {id}!")
-        finally:
-            print(f"Exiting {id}")
-
-    def evaluateGenome(self, genome: Genome, seed: int = 0, draw: bool = False) -> float:
-        """Load genome into boids environment and calculate a fitness score."""
-        # Load network with weights from genome
-        net = self.createNet()
-        net.setWeights(genome)
-
-        # Run network on boids environment
-        observations = self.env.reset()
-        done = False
-        cumulative_reward = 0
-        while not done:
-            if draw:
-                self.env.render()
-            # Collect actions for all agents with each agent using the same genome to guide its action
-            actions = {agent_name: net.run(torch.tensor(np.array([observations[agent_name]]), dtype=torch.float)) for agent_name in self.env.possible_agents}
-            # Step forward the environment
-            observations, rewards, dones, _  = self.env.step(actions)
-            # Save done
-            done = True in dones.values()
-            # Add the team reward to the cumulative reward
-            # Need [0] index because rewards are an array of rewards. One for each objective.
-            cumulative_reward += rewards["team"][0]
-        self.env.close()
-        return cumulative_reward
+    def randomGenome(self):
+        # Create a NN with random weights and get the weights as the genome
+        return NN(num_inputs=self.input_size, num_hidden=self.hidden_size, num_outputs=self.out_size).getWeights()
 
     def mutateGenome(self, genome: Genome) -> Genome:
         """Mutate weights of genome with zero-mean gaussian noise."""
         new_genome = []
         for layer in genome:
-            new_genome.append(layer + torch.normal(mean=0.0, std=self.sigma_mutation, size=(layer.shape)))
+            new_genome.append(layer + np.random.normal(0.0, self.sigma_mutation, size=(layer.shape)))
         return new_genome
 
     def mutatePopulation(self, scores) -> List[Genome]:
         """Generate a new population based on the fitness scores of the genomes in the population."""
-        print("scores: ", scores)
-        if scores[0] == scores[1]:
-            print('f')
-        # print("G: ", type(genome))
         # Sort population so that lowest scoring genomes are at the front of the list
         sorted_population = [genome for _, _, genome in sorted(zip(scores, list(range(len(self.population))), self.population))]
-        # sorted_population = [genome for _, genome in sorted(self.population, key=scores)]
 
         # Trying to minimize distance of swarm to objective, so lower scores are better
         # Keep parents with lowest scores
@@ -181,15 +176,15 @@ class Learner():
 
         # Queue genomes to be evaluated by workers
         seed = generateSeed()
-        for i, genome in enumerate(population):
-            self.work_queue.put({"id": i, "genome": genome, "seed": seed})
+        for genome_id, genome in enumerate(population):
+            self.work_queue.put({"id": genome_id, "genome": genome, "seed": seed})
 
         # Keep track of which genomes' fitnesses have been received
         received = [False for _ in population]
         timeout = 10 # seconds
         fitnesses = [np.inf for _ in population]
 
-        while not all (received):
+        while not all (received) and not self.stop_event.is_set():
             try:
                 # Grab results from workers
                 rx = self.fitness_queue.get(timeout=timeout)
@@ -208,6 +203,9 @@ class Learner():
                 print(
                     f"Work: {self.work_queue.qsize()}, Fitnesses: {self.fitness_queue.qsize()}"
                 )
+        if self.stop_event.is_set():
+            print("Stop event detected. Exiting main program.")
+            exit()
         return fitnesses
 
     def step(self) -> float:
