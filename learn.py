35162bd22902ad4c887c813a3193d65d08a8b3a5
EverardoG <everardo.a.gonzalez@gmail.com>
2022-09-30 16:36:06 -0700

    fix config

--- learn.py
+++ learn.py
@@ -11,9 +11,10 @@ team_size = 15
 num_learners =  np.arange(team_size)+1
 
 for num_learn in num_learners:
+    print(num_learn, team_size - num_learn)
     for _ in range(3):
-        config["CCEA"]["config"]["BoidsEnv"]["config"]["StateBounds"]["num_leaders"] = num_learn
-        config["CCEA"]["config"]["BoidsEnv"]["config"]["StateBounds"]["num_followers"] = team_size - num_learn
+        config["CCEA"]["config"]["BoidsEnv"]["config"]["StateBounds"]["num_leaders"] = int(num_learn)
+        config["CCEA"]["config"]["BoidsEnv"]["config"]["StateBounds"]["num_followers"] = int(team_size - num_learn)
         runExperiment(config)
 exit()
 
