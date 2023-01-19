514d5d0155634cb66c8d80ae400547d35308ccf9
EverardoG <everardo.a.gonzalez@gmail.com>
2023-01-19 13:59:37 -0800

    traj vis checkpoint

--- leader_follower/traj.py
+++ leader_follower/traj.py
@@ -1,12 +1,13 @@
 from pathlib import Path
+from typing import Optional
 
-from pandas import DataFrame
+from pandas import DataFrame, read_csv
 
 from leader_follower import project_properties
 from leader_follower.leader_follower_env import LeaderFollowerEnv
 
 def save_trajectories(env: LeaderFollowerEnv):
-    print(env.leaders["leader_0"].state_history)
+    # print(env.leaders["leader_0"].state_history)
     traj_dict = {
         leader_name: {
             "state_history": leader.state_history,
@@ -30,3 +31,15 @@ def save_trajectories(env: LeaderFollowerEnv):
     trajname = Path(traj_dir,f'trajectories_{num_trajs}.csv')
 
     traj_df.to_csv(trajname)
+
+def load_trajectories(traj_num: Optional[int] = None):
+    traj_dir = Path(project_properties.output_dir, 'trajs')
+    if not traj_dir.exists():
+        raise Exception("Could not load trajectories because trajectories folder was not found.")
+    
+    if traj_num is None:
+        traj_num = len(list(traj_dir.iterdir())) - 1
+    
+    trajname = Path(traj_dir,f'trajectories_{traj_num}.csv')
+
+    return read_csv(trajname)
---
+++ scripts/display_trajectories.py
@@ -0,0 +1,5 @@
+from leader_follower.traj import load_trajectories
+
+traj_df = load_trajectories()
+
+print(traj_df)
